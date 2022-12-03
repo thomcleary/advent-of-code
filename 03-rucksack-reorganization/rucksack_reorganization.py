d79f2d5c18907ebdc0503a89336e4fe62fda93c7
Thomas Cleary <thomasclearydev@gmail.com>
2022-12-03 16:16:23 +0800

    Day 3: Part 2

--- 03-rucksack-reorganization/rucksack_reorganization.py
+++ 03-rucksack-reorganization/rucksack_reorganization.py
@@ -3,27 +3,31 @@ Day 3: Rucksack Reorganization
 """
 
 from dataclasses import dataclass
+from typing import Iterator, Callable
 
 
 @dataclass
 class Rucksack:
-    leftCompartment: str
-    rightCompartment: str
+    contents: str
+    left_compartment: str
+    right_compartment: str
 
 
 def get_rucksack(rucksack: str) -> Rucksack:
     items_per_compartment = len(rucksack) // 2
-    return Rucksack(rucksack[:items_per_compartment], rucksack[items_per_compartment:])
+    return Rucksack(
+        rucksack, rucksack[:items_per_compartment], rucksack[items_per_compartment:]
+    )
 
 
 def get_rucksacks() -> list[Rucksack]:
     with open("puzzle-input.txt", "r") as puzzle_input:
-        return [get_rucksack(rucksack) for rucksack in puzzle_input.read().split("\n")]
+        return list(map(get_rucksack, puzzle_input.read().split("\n")))
 
 
 def get_item_type_in_both_compartments(rucksack: Rucksack) -> str:
-    item_in_both_compartments = set(rucksack.leftCompartment).intersection(
-        set(rucksack.rightCompartment)
+    item_in_both_compartments = set(rucksack.left_compartment).intersection(
+        set(rucksack.right_compartment)
     )
 
     if len(item_in_both_compartments) > 1:
@@ -32,10 +36,27 @@ def get_item_type_in_both_compartments(rucksack: Rucksack) -> str:
     return item_in_both_compartments.pop()
 
 
+def get_item_type_in_all_rucksacks(rucksacks: list[Rucksack]) -> str:
+    item_in_all_rucksacks: set[str] = set.intersection(
+        *(map(lambda r: set(r.contents), rucksacks))
+    )
+
+    if len(item_in_all_rucksacks) > 1:
+        raise Exception("Found more than 1 item in the group's rucksacks")
+
+    return item_in_all_rucksacks.pop()
+
+
 def get_item_type_priority(item_type: str) -> int:
     item_a, item_a_priority = ("A", 27) if item_type.upper() == item_type else ("a", 1)
-    ordDiff = ord(item_a) - item_a_priority
-    return ord(item_type) - ordDiff
+    ord_diff = ord(item_a) - item_a_priority
+    return ord(item_type) - ord_diff
+
+
+def groups(rucksacks: list[Rucksack]) -> Iterator[list[Rucksack]]:
+    group_size = 3
+    for i in range(0, len(rucksacks), group_size):
+        yield rucksacks[i : i + group_size]
 
 
 def main() -> None:
@@ -44,9 +65,20 @@ def main() -> None:
         "Part 1:",
         sum(
             map(
-                get_item_type_priority,
-                map(get_item_type_in_both_compartments, get_rucksacks()),
-            )
+                lambda r: get_item_type_priority(get_item_type_in_both_compartments(r)),
+                get_rucksacks(),
+            ),
+        ),
+    )
+
+    # Part 2
+    print(
+        "Part 2:",
+        sum(
+            map(
+                lambda g: get_item_type_priority(get_item_type_in_all_rucksacks(g)),
+                list(groups(get_rucksacks())),
+            ),
         ),
     )
 
--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 [Advent of Code - 2022](https://adventofcode.com/2022)
 
-| Day | Challenge           | Language  | Stars  |
-| :-: | ------------------- | :-------: | :----: |
-|  1  | Calorie Counting    | 🐍 Python | ⭐️⭐️ |
-|  2  | Paper Scissors Rock | 🐍 Python | ⭐️⭐️ |
+| Day | Challenge               | Language  | Stars  |
+| :-: | ----------------------- | :-------: | :----: |
+|  1  | Calorie Counting        | 🐍 Python | ⭐️⭐️ |
+|  2  | Paper Scissors Rock     | 🐍 Python | ⭐️⭐️ |
+|  3  | Rucksack Reorganization | 🐍 Python | ⭐️⭐️ |
