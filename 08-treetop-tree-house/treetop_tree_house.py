5328a9deededb392f701afb58825b4c86146881b
Thomas Cleary <thomasclearydev@gmail.com>
2022-12-10 11:03:14 +0800

    Day 8: Part 2

--- 08-treetop-tree-house/treetop_tree_house.py
+++ 08-treetop-tree-house/treetop_tree_house.py
@@ -4,39 +4,22 @@ Day 8: Treetop Tree House
 
 
 class Colour:
-    HEADER = "\033[95m"
-    OKBLUE = "\033[94m"
-    OKCYAN = "\033[96m"
-    OKGREEN = "\033[92m"
-    WARNING = "\033[93m"
-    FAIL = "\033[91m"
-    ENDC = "\033[0m"
     BOLD = "\033[1m"
-    UNDERLINE = "\033[4m"
-    LIGHT_GRAY = "\033[0;37m"
     DARK_GRAY = "\033[1;30m"
-    FAINT = "\033[2m"
+    ENDC = "\033[0m"
+    GREEN = "\033[92m"
+    WHITE = "\033[1;37m"
+    YELLOW = "\033[93m"
 
 
-TEST_INPUT = [
-    [int(tree) for tree in list(trees)]
-    for trees in """
-30373
-25512
-65332
-33549
-35390
-""".strip().split()
-]
-
-TreeMap = list[list[int]]
-
-
-def print_colour(output: str | int, colour: str, bold=False, end="\n") -> None:
+def print_colour(output: str | int, colour: str = Colour.WHITE, bold=False, end="\n") -> None:
     output = colour + str(output) + Colour.ENDC
     if bold:
         output = Colour.BOLD + output
     print(output, end=end)
+
+
+TreeMap = list[list[int]]
 
 
 def get_tree_map() -> TreeMap:
@@ -48,40 +31,74 @@ def is_map_edge(row: int, col: int, tree_map: TreeMap) -> bool:
     return row == 0 or col == 0 or row == len(tree_map[0]) - 1 or col == len(tree_map) - 1
 
 
-def is_visible(row: int, col: int, tree_map: TreeMap) -> bool:
+def get_tree_stats(row: int, col: int, tree_map: TreeMap) -> tuple[bool, int]:
+    if is_map_edge(row, col, tree_map):
+        return True, 0
+
     tree_map_transpose: TreeMap = list(map(list, zip(*tree_map)))
     tree = tree_map[row][col]
 
-    if is_map_edge(row, col, tree_map):
-        return True
+    west_trees = list(reversed(tree_map[row][0:col]))
+    east_trees = tree_map[row][col + 1 :]
+    north_trees = list(reversed(tree_map_transpose[col][:row]))
+    south_trees = tree_map_transpose[col][row + 1 :]
 
-    west_tree_max = max(tree_map[row][0:col])
-    east_tree_max = max(tree_map[row][col + 1 :])
-    north_tree_max = max(tree_map_transpose[col][:row])
-    south_tree_max = max(tree_map_transpose[col][row + 1 :])
+    is_visible = tree > min(max(west_trees), max(east_trees), max(south_trees), max(north_trees))
 
-    return tree > west_tree_max or tree > east_tree_max or tree > north_tree_max or tree > south_tree_max
+    scenic_score = 1
+    for direction in [west_trees, east_trees, north_trees, south_trees]:
+        distance = 0
+        for visible_tree in direction:
+            distance += 1
+            if visible_tree >= tree:
+                break
+        scenic_score *= distance
+
+    return is_visible, scenic_score
 
 
-def find_num_visibile(tree_map: TreeMap) -> int:
+def get_tree_map_stats(tree_map: TreeMap) -> tuple[int, int]:
     num_visible = 0
+    best_scenic_score = 0
 
+    for row, trees in enumerate(tree_map):
+        for col in range(len(trees)):
+            is_visible, scenic_score = get_tree_stats(row, col, tree_map)
+            if scenic_score > best_scenic_score:
+                best_scenic_score = scenic_score
+            if is_visible:
+                num_visible += 1
+
+    return num_visible, best_scenic_score
+
+
+def print_map_result(tree_map: TreeMap, max_scenic_score: int) -> None:
     print()
     for row, trees in enumerate(tree_map):
         for col, tree in enumerate(trees):
-            if is_visible(row, col, tree_map):
-                print_colour(tree, Colour.OKGREEN, True, end="")
-                num_visible += 1
+            is_visible, scenic_score = get_tree_stats(row, col, tree_map)
+            if scenic_score == max_scenic_score:
+                print_colour(tree, Colour.YELLOW, bold=True, end="")
+            elif is_visible:
+                print_colour(tree, Colour.GREEN, end="")
             else:
-                print_colour(tree, Colour.FAINT, end="")
+                print_colour(tree, Colour.DARK_GRAY, end="")
         print()
     print()
 
-    return num_visible
-
 
 def main() -> None:
-    print("Part 1:", find_num_visibile(get_tree_map()))
+    tree_map = get_tree_map()
+    num_trees_visible, best_scenic_score = get_tree_map_stats(tree_map)
+
+    # Part 1
+    print("Part 1:", num_trees_visible)
+
+    # Part 2
+    print("Part 2:", best_scenic_score)
+
+    # Visualisation
+    print_map_result(tree_map, best_scenic_score)
 
 
 if __name__ == "__main__":
