a7b24f5218a0a7c3c33624367e024462912dd917
Thomas Cleary <thomasclearydev@gmail.com>
2023-12-17 14:14:15 +0800

    2023 Day 12: Part 2

--- 2023/12-hot-springs/index.ts
+++ 2023/12-hot-springs/index.ts
@@ -2,10 +2,12 @@ import { getPuzzleInput, logChallenge, toLines } from "../utils.js";
 
 type ConditionRecord = { record: string; groups: number[] };
 
+const useExample = false;
+
 const OPERATIONAL = ".";
 const DAMAGED = "#";
 
-const useExample = false;
+const cache = new Map<string, number>();
 
 const conditionRecords = (): ConditionRecord[] => {
   return toLines(getPuzzleInput(import.meta.url, { useExample }))
@@ -18,63 +20,71 @@ const conditionRecords = (): ConditionRecord[] => {
     });
 };
 
+const unfoldRecords = (records: ConditionRecord[]): ConditionRecord[] =>
+  records.map(({ record, groups }) => ({
+    record: Array(5).fill(record).join("?"),
+    groups: Array(5)
+      .fill(groups)
+      .flatMap((g) => g),
+  }));
+
 const numberOfCombinations = ({ record, groups }: ConditionRecord, counting: boolean = false): number => {
+  const cacheKey = `record:${record};groups:${groups.join(",")};counting:${counting.toString()}`;
+
+  if (cache.has(cacheKey)) {
+    return cache.get(cacheKey)!;
+  }
+
   const groupsCopy = [...groups];
   const currentSpring = record.charAt(0);
-  const nextSpring = record.charAt(1);
+  const nextSpringIsDamaged = record.charAt(1).startsWith(DAMAGED);
+
+  let combinations: number;
 
   if (record === "") {
-    return counting || groups.length > 0 ? 0 : 1;
-  }
-
-  if (currentSpring === OPERATIONAL) {
-    if (counting) {
-      return 0;
+    combinations = groups.length > 0 ? 0 : 1;
+  } else if (currentSpring === OPERATIONAL) {
+    combinations = counting ? 0 : numberOfCombinations({ record: record.slice(1), groups }, false);
+  } else if (currentSpring === DAMAGED) {
+    if (groupsCopy[0] === 1) {
+      combinations = nextSpringIsDamaged
+        ? 0
+        : numberOfCombinations({ record: record.slice(2), groups: groupsCopy.slice(1) }, false);
+    } else {
+      groupsCopy[0]--;
+      combinations = numberOfCombinations({ record: record.slice(1), groups: groupsCopy }, true);
     }
+  } else {
+    const operationalNext = counting ? 0 : numberOfCombinations({ record: record.slice(1), groups: groupsCopy }, false);
 
-    return numberOfCombinations({ record: record.slice(1), groups }, false);
-  }
-
-  if (currentSpring === DAMAGED) {
-    --groupsCopy[0];
-
-    if (groupsCopy[0] === 0) {
-      if (nextSpring.startsWith(DAMAGED)) {
-        return 0;
-      }
-      groupsCopy.shift();
-      return numberOfCombinations({ record: record.slice(2), groups: groupsCopy }, false);
+    if (groupsCopy[0] === 1) {
+      combinations =
+        operationalNext +
+        (nextSpringIsDamaged
+          ? 0
+          : numberOfCombinations({ record: record.slice(2), groups: groupsCopy.slice(1) }, false));
+    } else {
+      groupsCopy[0]--;
+      combinations = operationalNext + numberOfCombinations({ record: record.slice(1), groups: groupsCopy }, true);
     }
-
-    return numberOfCombinations({ record: record.slice(1), groups: groupsCopy }, true);
   }
 
-  const whenOperationalNext = counting
-    ? 0
-    : numberOfCombinations({ record: record.slice(1), groups: [...groupsCopy] }, false);
-
-  if (groupsCopy[0] === 1) {
-    if (nextSpring.startsWith(DAMAGED)) {
-      return whenOperationalNext + 0;
-    }
-
-    groupsCopy.shift();
-    return whenOperationalNext + numberOfCombinations({ record: record.slice(2), groups: groupsCopy }, false);
-  }
-
-  --groupsCopy[0];
-  return whenOperationalNext + numberOfCombinations({ record: record.slice(1), groups: groupsCopy }, true);
+  cache.set(cacheKey, combinations);
+  return combinations;
 };
 
-const part1 = () => conditionRecords().reduce((prev, curr) => prev + numberOfCombinations(curr), 0);
+const combinationSum = (records: ConditionRecord[]) =>
+  records.reduce((prev, curr) => prev + numberOfCombinations(curr), 0);
 
-const part2 = () => {};
+const part1 = () => combinationSum(conditionRecords());
+
+const part2 = () => combinationSum(unfoldRecords(conditionRecords()));
 
 const hotSprings = () =>
   logChallenge({
     name: "Day 12: Hot Springs",
     part1: { run: part1, expected: useExample ? 21 : 6488 },
-    part2: { run: part2, expected: undefined },
+    part2: { run: part2, expected: useExample ? 525152 : 815364548481 },
   });
 
 hotSprings();
