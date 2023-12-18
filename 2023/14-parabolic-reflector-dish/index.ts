fbf093e540fa9f224eaf5f9a7e6f67c420e3e683
Thomas Cleary <thomasclearydev@gmail.com>
2023-12-18 18:54:29 +0800

    2023 Day 14: Part 2

--- 2023/14-parabolic-reflector-dish/index.ts
+++ 2023/14-parabolic-reflector-dish/index.ts
@@ -8,36 +8,72 @@ const symbol = {
   empty: ".",
 } as const;
 
+const notRoundRegex = new RegExp(`[^${symbol.round}]`, "g");
+
 const platform = toLines(getPuzzleInput(import.meta.url, { useExample }));
 
 const platformTranspose = (platform: string[]) =>
   transpose(platform.map((row) => row.split(""))).map((column) => column.join(""));
 
-const tiltNorth = (platform: string[]) => {
-  const pTranspose = platformTranspose(platform);
-  pTranspose.forEach((column, index) => {
-    const sections = column.split(symbol.cube).map((symbols) => symbols.split(""));
-    sections.forEach((section) => section.sort((a, b) => (a === b ? 0 : b === symbol.round ? 1 : -1)));
-    pTranspose[index] = sections.map((s) => s.join("")).join(symbol.cube);
-  });
-  return platformTranspose(pTranspose);
+const tiltPlatform = (platform: string[], direction: "west" | "east") =>
+  platform.map((row) =>
+    row
+      .split(symbol.cube)
+      .map((symbols) => {
+        const numRound = symbols.replaceAll(notRoundRegex, "").length;
+        const rounds = symbol.round.repeat(numRound);
+        const empties = symbol.empty.repeat(symbols.length - numRound);
+        return direction === "west" ? rounds + empties : empties + rounds;
+      })
+      .join(symbol.cube),
+  );
+
+const tiltWest = (platform: string[]) => tiltPlatform(platform, "west");
+const tiltNorth = (platform: string[]) => platformTranspose(tiltWest(platformTranspose(platform)));
+const tiltEast = (platform: string[]) => tiltPlatform(platform, "east");
+const tiltSouth = (platform: string[]) => platformTranspose(tiltEast(platformTranspose(platform)));
+
+const cycle = (platform: string[]) => tiltEast(tiltSouth(tiltWest(tiltNorth(platform))));
+
+const northLoad = (platform: string[]) =>
+  platform.reverse().reduce((prev, curr, index) => prev + curr.replaceAll(notRoundRegex, "").length * (index + 1), 0);
+
+const part1 = () => northLoad(tiltNorth(platform));
+
+const part2 = () => {
+  const cyclesToPerform = 1_000_000_000;
+  const cache = new Map<string, { next: string[]; cycle: number }>();
+
+  let p = platform;
+  let c = 0;
+
+  for (; c < cyclesToPerform; c++) {
+    const cacheKey = p.join();
+
+    if (cache.has(cacheKey)) {
+      break;
+    } else {
+      const next = cycle(p);
+      cache.set(cacheKey, { next, cycle: c });
+      p = next;
+    }
+  }
+
+  const cyclesRemaining = cyclesToPerform - c;
+  const repeatedCycleLength = c - cache.get(p.join())!.cycle;
+
+  for (let i = 0; i < cyclesRemaining % repeatedCycleLength; i++) {
+    p = cache.get(p.join())!.next;
+  }
+
+  return northLoad(p);
 };
-
-const part1 = () =>
-  tiltNorth(platform)
-    .reverse()
-    .reduce(
-      (prev, curr, index) => prev + curr.replaceAll(new RegExp(`[^${symbol.round}]`, "g"), "").length * (index + 1),
-      0,
-    );
-
-const part2 = () => {};
 
 const parabolicReflectorDish = () =>
   logChallenge({
     name: "Day 14: Parabolic Reflector Dish",
     part1: { run: part1, expected: useExample ? 136 : 105003 },
-    part2: { run: part2, expected: undefined },
+    part2: { run: part2, expected: useExample ? 64 : 93742 },
   });
 
 parabolicReflectorDish();
