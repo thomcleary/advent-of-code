a0bc3a6264a99eb89d085ec82ffc4c79a94ce80a
thomcleary <thomasclearydev@gmail.com>
2024-09-21 16:19:23 +0800

    2015 Day 2: Part 1

---
+++ 2015/02-i-was-told-there-would-be-no-math/README.md
@@ -0,0 +1,13 @@
+# Day 1: I Was Told There Would Be No Math
+
+## Compile
+
+```shell
+cc part.c -o part
+```
+
+## Run
+
+```shell
+./part < input.txt
+```
---
+++ 2015/02-i-was-told-there-would-be-no-math/part1.c
@@ -0,0 +1,39 @@
+/*
+Day 2: I Was Told There Would Be No Math (Part 1)
+https://adventofcode.com/2015/day/2
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int smallest_side(int x, int y, int z);
+
+int main(void)
+{
+    int wrapping_paper_feet = 0;
+    int length, height, width;
+
+    while (scanf("%dx%dx%d", &length, &height, &width) != EOF)
+    {
+        int lw = length * width;
+        int wh = width * height;
+        int hl = height * length;
+
+        wrapping_paper_feet += (2 * lw) + (2 * wh) + (2 * hl);
+        wrapping_paper_feet += smallest_side(lw, wh, hl);
+    }
+
+    printf("Wrapping paper required (feet): %d\n", wrapping_paper_feet);
+
+    return 0;
+}
+
+int smallest_side(int x, int y, int z)
+{
+    if (x < y && x < z)
+    {
+        return x;
+    }
+
+    return y < z ? y : z;
+}
