5acac5f7d9d1b219ccb0a57b5efb245eb11d5d29
thomcleary <thomasclearydev@gmail.com>
2024-10-03 09:16:17 +0800

    2015 Day 5: Part 2

---
+++ 2015/04-the-ideal-stocking-stuffer/README.md
@@ -0,0 +1,15 @@
+# Day 4: The Ideal Stocking Stuffer
+
+<https://adventofcode.com/2015/day/4>
+
+## Compile
+
+```shell
+cc part.c -o part
+```
+
+## Run
+
+```shell
+./part < input.txt
+```
---
+++ 2015/05-doesnt-he-have-intern-elves-for-this/part2.c
@@ -0,0 +1,72 @@
+/*
+Day 5: Doesn't He Have Intern-Elves For This? (Part 2)
+https://adventofcode.com/2015/day/5
+*/
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_STR_LEN 16
+
+bool is_nice(char *str);
+
+int main(void)
+{
+    char str[MAX_STR_LEN + 2]; // +1 for '\n', +1 for '\0'
+    int nice = 0;
+
+    while (fgets(str, MAX_STR_LEN + 2, stdin) != NULL)
+    {
+        if (is_nice(str))
+        {
+            nice++;
+        }
+    }
+
+    printf("Nice strings: %d\n", nice);
+
+    return 0;
+}
+
+void to_lower(char *str)
+{
+    while (*str)
+    {
+        *str = tolower(*str);
+        str++;
+    }
+}
+
+bool is_nice(char *str)
+{
+    to_lower(str);
+
+    bool has_repeat_char = false;
+    bool has_repeat_substr = false;
+
+    char needle[3] = {'\0'};
+
+    while (*(str + 2) && *(str + 2) != '\n')
+    {
+        char *haystack = str + 2;
+
+        if (*str == *haystack)
+        {
+            has_repeat_char = true;
+        }
+
+        needle[0] = *str;
+        needle[1] = *(str + 1);
+
+        if (strstr(haystack, needle) != NULL)
+        {
+            has_repeat_substr = true;
+        }
+
+        str++;
+    }
+
+    return has_repeat_char && has_repeat_substr;
+}
