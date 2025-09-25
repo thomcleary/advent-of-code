48d94802d0e0e6050fe61c21d581f88cbc1b577c
thomcleary <thomasclearydev@gmail.com>
2025-09-25 11:45:20 +0800

    2015 Day 1: Part 2 (Haskell)

--- 2015/haskell/01-not-quite-lisp/Main.hs
+++ 2015/haskell/01-not-quite-lisp/Main.hs
@@ -1,8 +1,22 @@
+toLevel :: Char -> Int
+toLevel '(' = 1
+toLevel ')' = negate 1
+
 part1 :: String -> Int
-part1 = sum . map (\d -> if d == '(' then 1 else -1)
+part1 = sum . map toLevel
+
+part2 :: String -> Int
+part2 directions = go 0 0 (map toLevel directions)
+  where
+    go i (-1) ds = i
+    go i floor (d : ds) = go (i + 1) (floor + d) ds
 
 main :: IO ()
 main = do
   input <- getContents
-  putStrLn ("Part 1: " ++ show (part1 input))
-  putStrLn ("Part 2: " ++ "TODO")
+
+  let p1 = part1 input
+  let p2 = part2 input
+
+  putStrLn ("Part 1: " ++ show p1)
+  putStrLn ("Part 2: " ++ show p2)
