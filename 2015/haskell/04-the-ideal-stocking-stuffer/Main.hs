88e1fb1bd77baff696ec951b34a557f3f2cc044c
thomcleary <thomasclearydev@gmail.com>
2025-09-25 17:12:11 +0800

    2015 Day 4: Part 2 (Haskell)

--- 2015/haskell/04-the-ideal-stocking-stuffer/Main.hs
+++ 2015/haskell/04-the-ideal-stocking-stuffer/Main.hs
@@ -2,19 +2,20 @@ import Data.Digest.Pure.MD5 (md5)
 import Data.List (isPrefixOf)
 import qualified Data.String as ByteString
 
-mine :: String -> Int
-mine key = go 1
+mine :: String -> Int -> Int
+mine key zeros = go 1
   where
+    isMatch = isPrefixOf $ replicate zeros '0'
     go n
-      | "00000" `isPrefixOf` show (md5 (ByteString.fromString (key ++ show n))) = n
+      | isMatch $ show (md5 (ByteString.fromString (key ++ show n))) = n
       | otherwise = go (n + 1)
 
 main :: IO ()
 main = do
   key <- getContents
 
-  let part1 = mine key
-  let part2 = "TODO"
+  let part1 = mine key 5
+  let part2 = mine key 6
 
   putStrLn ("Part 1: " ++ show part1)
   putStrLn ("Part 2: " ++ show part2)
