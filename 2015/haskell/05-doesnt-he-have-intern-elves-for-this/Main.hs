4cb04ce69e67a682129a0a5e9cd13be065ed82f2
thomcleary <thomasclearydev@gmail.com>
2025-09-26 09:15:12 +0800

    2015 Day 5: Part 2 (Haskell)

--- 2015/haskell/05-doesnt-he-have-intern-elves-for-this/Main.hs
+++ 2015/haskell/05-doesnt-he-have-intern-elves-for-this/Main.hs
@@ -10,11 +10,22 @@ hasRepeatingCharacter _ = False
 hasNaughtySubstring :: String -> Bool
 hasNaughtySubstring str = any (`isInfixOf` str) ["ab", "cd", "pq", "xy"]
 
+hasDuplicatePair :: String -> Bool
+hasDuplicatePair (x : y : rest) = [x, y] `isInfixOf` rest || hasDuplicatePair (y : rest)
+hasDuplicatePair _ = False
+
+hasRepeatingSeparatedCharacter :: String -> Bool
+hasRepeatingSeparatedCharacter (x : y : z : rest) = x == z || hasRepeatingSeparatedCharacter (y : z : rest)
+hasRepeatingSeparatedCharacter _ = False
+
 isNice :: String -> Bool
 isNice str =
   hasAtleastThreeVowels str
     && hasRepeatingCharacter str
     && not (hasNaughtySubstring str)
+
+isNicer :: String -> Bool
+isNicer str = hasDuplicatePair str && hasRepeatingSeparatedCharacter str
 
 main :: IO ()
 main = do
@@ -23,7 +34,7 @@ main = do
   let strs = lines input
 
   let part1 = length $ filter isNice strs
-  let part2 = "TODO"
+  let part2 = length $ filter isNicer strs
 
   putStrLn ("Part 1: " ++ show part1)
   putStrLn ("Part 2: " ++ show part2)
