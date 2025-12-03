82afaf4993b5b5ba155062a3467811cd7db693b6
thomcleary <thomasclearydev@gmail.com>
2025-12-03 15:40:23 +0800

    2025 Day 2: Part 2

--- 2025/02-gift-shop/index.ts
+++ 2025/02-gift-shop/index.ts
@@ -6,8 +6,42 @@ type Range = {
   end: number;
 };
 
-const toRanges = (stdin: string) => {
-  return stdin
+const invalidIdSum = (ranges: Range[], { maxRepeats }: { maxRepeats?: number } = {}): number => {
+  return ranges
+    .flatMap((range) => {
+      let invalid: number[] = [];
+
+      for (let id = range.start; id <= range.end; id++) {
+        const digits = id.toString();
+
+        for (let repeats = 2; repeats <= (maxRepeats ?? digits.length); repeats++) {
+          if (digits.length % repeats !== 0) {
+            continue; // could increment i until there is another digit
+          }
+
+          const chunkSize = digits.length / repeats;
+          const chunks = new Set<string>();
+
+          for (let i = 0; i < digits.length; i += chunkSize) {
+            chunks.add(digits.slice(i, i + chunkSize));
+          }
+
+          if (chunks.size === 1) {
+            invalid.push(id);
+            break;
+          }
+        }
+      }
+
+      return invalid;
+    })
+    .reduce((a, b) => a + b, 0);
+};
+
+const main = async () => {
+  const stdin = await getStdin();
+
+  const ranges = stdin
     .trim()
     .split(",")
     .map((r) => {
@@ -15,42 +49,9 @@ const toRanges = (stdin: string) => {
       assert(start && end);
       return { start: parseInt(start), end: parseInt(end) } satisfies Range;
     });
-};
 
-const findInvalidIds = (ranges: Range[]): number[] => {
-  return ranges.flatMap((range) => {
-    let invalid: number[] = [];
-
-    for (let i = range.start; i <= range.end; i++) {
-      const digits = i.toString();
-
-      if (digits.length % 2 !== 0) {
-        continue; // could increment i until there is another digit
-      }
-
-      const middle = digits.length / 2;
-      const left = digits.slice(0, middle);
-      const right = digits.slice(middle);
-
-      if (left === right) {
-        invalid.push(i);
-      }
-    }
-
-    return invalid;
-  });
-};
-
-const main = async () => {
-  const stdin = await getStdin();
-
-  const ranges = toRanges(stdin);
-
-  const invalidIds = findInvalidIds(ranges);
-  const invalidIdSum = invalidIds.reduce((prev, curr) => prev + curr, 0);
-
-  console.log(`Part 1: ${invalidIdSum}`);
-  console.log(`Part 2: TODO`);
+  console.log(`Part 1: ${invalidIdSum(ranges, { maxRepeats: 2 })}`);
+  console.log(`Part 2: ${invalidIdSum(ranges)}`);
 };
 
 await main();
